501e3aebb610680169e7e42bd5f1d03976f54cd5
alex-panarin <alex.panarin.69@mail.ru>
2020-12-20 06:58:57 +0300

    using MemoryMappedFile

--- XmlParserTest/Program.cs
+++ XmlParserTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.MemoryMappedFiles;
 
 namespace XmlParserTest
 {
@@ -21,20 +22,22 @@ namespace XmlParserTest
                  * Для работы с файлом большого объема лучше использовать MemoryMappedFile
                  * И делать параллельную сборку Нод используя Parallel и CreateViewAccessor
                 */
-                using (var reader = new StreamReader("Example.xml"))
+                //using var reader = new StreamReader("Example.xml");
+                using var file = MemoryMappedFile.CreateFromFile("Example.xml");
+                using var reader =  new StreamReader(file.CreateViewStream());
+                
+                List<KeyValuePair<string, Func<string, Node>>> templates = new List<KeyValuePair<string, Func<string, Node>>>
                 {
-                    List<KeyValuePair<string, Func<string, Node>>> templates = new List<KeyValuePair<string, Func<string, Node>>>
-                    {
-                         new KeyValuePair<string, Func<string, Node>>("cim:Substation", (s) => new SubstationNode(s)),
-                         new KeyValuePair<string, Func<string, Node>>("cim:VoltageLevel", (s) => new VoltageNode(s)),
-                         new KeyValuePair<string, Func<string, Node>>("cim:SynchronousMachine", (s) => new MachineNode(s)),
-                         //new KeyValuePair<string, Func<string, Node>>("cim:OperationalLimitSet", (s) => new LimitSetNode(s)), // Можно подключать и отключать Ноды
-                    };
+                        new KeyValuePair<string, Func<string, Node>>("cim:Substation", (s) => new SubstationNode(s)),
+                        new KeyValuePair<string, Func<string, Node>>("cim:VoltageLevel", (s) => new VoltageNode(s)),
+                        new KeyValuePair<string, Func<string, Node>>("cim:SynchronousMachine", (s) => new MachineNode(s)),
+                        //new KeyValuePair<string, Func<string, Node>>("cim:OperationalLimitSet", (s) => new LimitSetNode(s)), // Можно подключать и отключать Ноды
+                };
 
-                    node = new NodesRepository(reader, new NodeMediator(templates));
+                node = new NodesRepository(reader, new NodeMediator(templates));
                     
-                    node.CollectNodes();
-                }
+                node.CollectNodes();
+                
                 
                 var s = node?.ToJson();
                 
